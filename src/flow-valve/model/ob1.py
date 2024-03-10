6e5e7538a95d475d87e9e4006b494e40fa5c51a6
dwaynedmello <ddmello@student.ubc.ca>
2024-03-09 16:09:27 -0800

    implemented get_pressure and set_pressure functionality

--- src/flow-valve/model/ob1.py
+++ src/flow-valve/model/ob1.py
@@ -27,10 +27,20 @@ class Ob1FlowValveController(FlowValveController):
         OB1_Destructor(self.instrument_id)
 
     def __str__(self):
-        raise NotImplementedError("FlowValveController.__str__ is unimplemented")
+        #raise NotImplementedError("FlowValveController.__str__ is unimplemented")
+        return f"OB1 Controller {self.name}, ID: {self.instrument_id}"
 
     def get_pressure(self, channel):
-        raise NotImplementedError("FlowValveController.get_pressure is unimplemented")
+        #raise NotImplementedError("FlowValveController.get_pressure is unimplemented")
+        pressure = c_double()
+        # Assuming default calibration is loaded and calibration length is 1000
+        error = OB1_Get_Press(self.instrument_id, channel, 1, byref(self.calib), byref(pressure), 1000)
+        if error != 0:
+            raise Exception(f"Get pressure failed with error code: {error}")
+        return pressure.value
 
     def set_pressure(self, channel, pressure):
-        raise NotImplementedError("FlowValveController.set_pressure is unimplemented")
+        #raise NotImplementedError("FlowValveController.set_pressure is unimplemented")
+        error = OB1_Set_Press(self.instrument_id, channel, c_double(pressure), byref(self.calib), 1000)
+        if error != 0:
+            raise Exception(f"Set pressure failed with error code: {error}")
